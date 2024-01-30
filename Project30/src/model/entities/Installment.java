edbea97d2a2903dd9097d76f24b908b472ee083d
lucenac <joaopedrolucena568@gmail.com>
2024-01-30 16:53:37 -0300

    adding complete Project 30

--- Project30/src/model/entities/Installment.java
+++ Project30/src/model/entities/Installment.java
@@ -1,4 +1,38 @@
 package model.entities;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Installment {
+    private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    private LocalDate date;
+    private Double amount;
+
+    public Installment(){
+    }
+    public Installment(LocalDate dueDate, Double amount) {
+        this.date = dueDate;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return date.format(fmt) + " - " + String.format("%.2f", amount);
+    }
 }
